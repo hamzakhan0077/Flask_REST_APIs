11772c6a7ae48bd1c955d5547cdec768082549d8
Hamza Hassan Khan <hamza.334467@yahoo.com>
2023-08-02 13:23:12 +0100

    implement tests
    Signed-off-by: Hamza Hassan Khan <hamza.334467@yahoo.com>

--- REST_APIs/routes.py
+++ REST_APIs/routes.py
@@ -2,10 +2,6 @@ from REST_APIs import app
 from flask import render_template
 
 
-
 @app.route("/")
 def hello():
     return render_template("home.html")
-
-
-
--- test.py
+++ test.py
@@ -1,12 +1,6 @@
-import requests
-from flask_restful import fields, reqparse
+import requests, unittest
 
 BASE = 'http://127.0.0.1:5000/'
-
-# response = requests.get(BASE +"hello")
-# response2 = requests.post(BASE +"hello")
-# print(response.json())
-# print(response2.json())
 
 dummy_data = [
     {"id": 0, "name": "Alice", "email": "allice@hh.pt", "note": "This customer is due to receive a gift card"},
@@ -18,62 +12,68 @@ x = {"id": 4, "name": "Mario", "email": "Mario@gg.org", "note": "Hello, I am mar
 y = {"id": 5, "name": "Abby", "email": "abby@klm.nl", "note": "Invalid Gift Card"}
 
 
-def test():
-    # Inserting 4 Dummy Customers above
-    print("PUT (INSERT DATA)")
-    for i, d in enumerate(dummy_data):
-        res = requests.put(BASE + f"customers/{i}", json=d)  # use json attribute, or u will not json serializable error
-        print(res.json())
+class TestCustomerRestApi(unittest.TestCase):
 
-    # Getting those 4 Customers
-    print("\n GET (GET DATA) \n")
-    for i in range(4):
-        res2 = requests.get(BASE + f"customers/{i}")
-        print(res2.json())
+    def test1_put(self):
+        # Inserting 4 Dummy Customers above
+        for i, d in enumerate(dummy_data):
+            res = requests.put(BASE + f"customers/{i}",
+                               json=d)  # use json attribute, or u will not json serializable error
+            status_code = res.status_code
+            if status_code != 201:
+                self.assertEqual(status_code, 201)
 
-    # Updating Alice's email and her name
-    print("\nPATCH (UPDATE DATA) \n")
+    def test2_get(self):
+        # Getting those 4 Customers
+        for i in range(4):
+            res2 = requests.get(BASE + f"customers/{i}")
+            status_code = res2.status_code
+            if status_code != 200:
+                self.assertEqual(status_code, 200)
 
-    res3 = requests.patch(BASE + f"customers/0",
-                          json={"email": "Alice@skydiving.ie", "note": "Alice has updated her email"})
-    print(res3.json())
-    res4 = requests.get(BASE + f"customers/0")
-    print(res4.json())
+    def test3_patch(self):
+        # Updating Alice's email and her name
+        res3 = requests.patch(BASE + f"customers/0",
+                              json={"email": "Alice@skydiving.ie", "note": "Alice has updated her email"})
+        status_code = res3.status_code
+        self.assertEqual(status_code, 200)
+        # Res 4 deleted as it was unnecessary
 
-    # Inserting more dummy data to delete
-    print("\n Inserting data to delete \n")
-    res5 = requests.put(BASE + "customers/4", json=x)
-    res6 = requests.put(BASE + "customers/5", json=y)
-    print(res5.json())
-    print(res6.json())
+    def test4_delete(self):
+        # Inserting more dummy data to delete
+        requests.put(BASE + "customers/4", json=x)  # res5
+        requests.put(BASE + "customers/5", json=y)  # res6
+        # Deleting the dummy data
+        res8 = requests.delete(BASE + f"customers/4")  # res 8
+        res9 = requests.delete(BASE + f"customers/5")  # res 9
+        status_code_res8 = res8.status_code
+        status_code_res9 = res9.status_code
+        self.assertEqual(status_code_res8, 204)
+        self.assertEqual(status_code_res9, 204)
 
-    print("\n Final Data \n")
-    for j in range(6):
-        res7 = requests.get(BASE + f"customers/{j}")
-        print(res7.json())
+    def test5_not_found(self):
+        r = requests.get(BASE + f"customers/66")
+        status_code = r.status_code
+        self.assertEqual(status_code, 404)
 
-    # Deleting the dummy data
-    print("\n DELETE (DELETE DATA) \n")
-    requests.delete(BASE + f"customers/4")  # res 8
-    requests.delete(BASE + f"customers/5")  # res 9
+    def test6_already_exist(self):
+        r = requests.put(BASE + f"customers/1", json=x)
+        status_code = r.status_code
+        self.assertEqual(status_code, 409)
 
-    print("\n Data After Deletion \n")
-    for k in range(6):
-        res10 = requests.get(BASE + f"customers/{k}")
-        print(res10.json())
+    def test7_delete_unavailable_data(self):
+        r = requests.delete(BASE + f"customers/66")
+        status_code = r.status_code
+        self.assertEqual(status_code, 404)
 
-# Clears dummy data in the db thats left after running the test() , SO code can be re-run
-def clear_this_data():
-    for l in range(10):
-        res11 = requests.delete(BASE + f"customers/{l}")
+    # Clears dummy data in the db thats left after running the test() , SO code can be re-run
+    def test8_data_clear(self):
+        for l in range(4):
+            res11 = requests.delete(BASE + f"customers/{l}")
+            status_code_res11 = res11.status_code
+            if status_code_res11 != 204:
+                self.assertEqual(status_code_res11, 204)
 
 
 if __name__ == '__main__':
-    test()  # run the script, see the requests in the FLASK Dev Server running on Commandline
-    # clear_this_data()
-
-    #
-    # print("\n GET (GET DATA) \n")
-    # for i in range(4):
-    #     res2 = requests.get(BASE + f"customers/{i}")
-    #     print(res2.json())
+    unittest.main()
