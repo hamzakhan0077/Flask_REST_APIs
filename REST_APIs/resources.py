96fd64eb250b8427e77844c5e43b15ae75d95baa
hamzakhan0077 <hamza.334467@yahoo.com>
2024-12-18 14:46:17 +0000

    update customer resourse to include customer by id and customer

--- REST_APIs/resources.py
+++ REST_APIs/resources.py
@@ -1,10 +1,11 @@
 from flask_restful import Resource, reqparse, fields, marshal_with, abort
 from REST_APIs import api, db, app
 from REST_APIs.models import Customer
+import uuid
+import http
 
 # Request parser makes sure that the required data is sent with the request
 customer_args = reqparse.RequestParser()
-customer_args.add_argument("id", type=str, help="Customer ID is required", required=True)
 customer_args.add_argument("name", type=str, help="Customer name is required", required=True)
 customer_args.add_argument("email", type=str, help="Customer email required", required=True)
 customer_args.add_argument("note", type=str, help="Leave an optional note ")
@@ -12,7 +13,6 @@ customer_args.add_argument("note", type=str, help="Leave an optional note ")
 # update parser, so a user can update some specific info
 customer_update_args = reqparse.RequestParser()
 # no required attribute as args are optional
-customer_update_args.add_argument("id", type=str, help="Customer ID is required")
 customer_update_args.add_argument("name", type=str, help="Customer name is required")
 customer_update_args.add_argument("email", type=str, help="Customer email required")
 customer_update_args.add_argument("note", type=str, help="Leave an optional note ")
@@ -22,7 +22,7 @@ customer_update_args.add_argument("note", type=str, help="Leave an optional note
 # @marshall_with decorator, when we return, take the return value and serialize it with the resource field
 cust_resource_fields = {
 
-    'id': fields.Integer,
+    'id': fields.String,
     'name': fields.String,
     'email': fields.String,
     'note': fields.String
@@ -30,12 +30,12 @@ cust_resource_fields = {
 }
 
 
-class Customers(Resource):
+class Customers_By_Id(Resource):
     @marshal_with(cust_resource_fields)
     def get(self, id):
         res = Customer.query.filter_by(id=id).first()
         if not res:
-            abort(404, message="Customer not found")
+            abort(http.HTTPStatus.NOT_FOUND, message="Customer not found")
         return res
 
         # data = []
@@ -46,24 +46,13 @@ class Customers(Resource):
     def post(self):
         return {"data": "data Posted"}
 
-    @marshal_with(cust_resource_fields)
-    def put(self, id):
-        args = customer_args.parse_args()
-        res = Customer.query.filter_by(id=id).first()
-        if res:
-            abort(409, message="Already exists")
-        cust = Customer(id=id, name=args['name'], email=args['email'], note=args['note'])
-
-        db.session.add(cust)
-        db.session.commit()
-        return cust, 201
 
     @marshal_with(cust_resource_fields)
     def patch(self, id):
         args = customer_update_args.parse_args()
         res = Customer.query.filter_by(id=id).first()
         if not res:
-            abort(404, message="Customer not found")
+            abort(http.HTTPStatus.NOT_FOUND, message="Customer not found")
 
         for arg in args:
             if args[arg]:
@@ -78,11 +67,28 @@ class Customers(Resource):
     def delete(self, id):
         res = Customer.query.filter_by(id=id).first()
         if not res:
-            abort(404, message="Customer not found")
+            abort(http.HTTPStatus.NOT_FOUND, message="Customer not found")
         db.session.delete(res)
         db.session.commit()
-        return '', 204
+        return '',http.HTTPStatus.NO_CONTENT
 
 
-api.add_resource(Customers, "/customers/<int:id>")
-# api.add_resource(Customers,"/customers")
+class Customers(Resource):
+    @marshal_with(cust_resource_fields)
+    def put(self):
+        args = customer_args.parse_args()
+        cust = Customer(id=str(uuid.uuid4()), name=args['name'], email=args['email'], note=args['note'])
+
+        db.session.add(cust)
+        db.session.commit()
+        return cust, http.HTTPStatus.OK
+    
+    @marshal_with(cust_resource_fields)
+    def get(self):
+        res = Customer.query.all()
+        return res
+       
+
+
+api.add_resource(Customers_By_Id, "/customers/<string:id>")
+api.add_resource(Customers,"/customers")
